6673170a6029b937a62dba8c7d615acf414708fb
NICHOLAS MADEL <104400058+NicholasMadel@users.noreply.github.com>
2022-05-30 18:14:45 -0300

    Add files via upload

---
+++ Aula WEB/js/script.js
@@ -0,0 +1,23 @@
+//let n1 = parseFloat(prompt("Digite um número"))
+//let n2 = parseFloat(prompt("Digite outro número"))
+
+//let soma = n1 + n2
+
+//alert("Resultado da soma: " + soma)
+
+// let reinos = {
+//     reino1: "Animalia",
+//     reino2: "Fungi",
+//     reino3: "Pantae",
+//     reinos: ["Animalia", "Fungi", "Plantae"]
+// }
+
+// let Animal ={
+//     reino: reinos.reino1, //reino:reinos.nome[0]
+//     filo: "Chordata",
+//     classe:"Mamalia",
+//     ordem:"Carnivora"
+// }
+
+// console.log(Animal.reino)
+
